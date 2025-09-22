1baaee67f22d7c063ff101f7b0f2fcb8495f4bfd
Islam Abdalah <engislam252@gmail.com>
2025-09-22 21:56:06 +0300

    add express way with call new way and Pipes

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,4 +1,5 @@
-import { Body, Controller, Delete, Get, NotFoundException, Param, Post, Put } from '@nestjs/common';
+import { Body, Controller, Delete, Get, NotFoundException, Param, ParseIntPipe, Post, Put ,Req, Res} from '@nestjs/common';
+import express from 'express';
 import { CreateProductDto } from './dtos/create-product.dto';
 type ProductType = { id: number, name: string, price: number }
 @Controller('api/')
@@ -30,6 +31,16 @@ export class ProductsController {
             price: 450.0,
         },
     ];
+    @Post("product-with-express")
+    public createProductWithExpress(@Req() req: express.Request, @Res() res:express.Response) {
+        const newItem = {
+            id: this.products.length + 1,
+            name: req.body.name,
+            price: req.body.price
+        }
+        this.products.push(newItem);
+        res.status(201).json(newItem);
+    }
     @Post("product")
     public createProduct(@Body() body: CreateProductDto) {
         const newItem = {
@@ -46,23 +57,23 @@ export class ProductsController {
         return this.products
     }
 
-    @Get("products/:id")
-    public getProductById(@Param("id") id: string){
+    @Get("products/:id", )
+    public getProductById(@Param("id",ParseIntPipe) id: number){
         return this.getProductObjectById(id)
     }
     @Put("products/:id")
-    public UpdateProductById(@Param("id") id: string){
+    public UpdateProductById(@Param("id",ParseIntPipe) id: number){
         let selectProduct = this.getProductObjectById(id);
         return {message:'product updated successfully with id: '+id}
     }
     @Delete("products/:id")
-    public DeleteProductById(@Param("id") id: string){
+    public DeleteProductById(@Param("id",ParseIntPipe) id: number){
         this.getProductObjectById(id);
         return {message:'product is Delete successfully with id: '+id}
     }
 
-    getProductObjectById(id:string):ProductType{
-        let product = this.products.find(product => product.id == parseInt(id));
+    getProductObjectById(id:number):ProductType{
+        let product = this.products.find(product => product.id == id);
         if(!product) throw new NotFoundException("Product not found");
         return product
     }
