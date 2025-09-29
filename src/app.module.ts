d1281ffce9b42963b0fccff49e5c4b4eef032726
Islam Abdalah <engislam252@gmail.com>
2025-09-29 23:23:22 +0300

    updated packages

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,23 +4,49 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
 import { ReviewsModule } from './reviews/reviews.module';
-import {TypeOrmModule} from '@nestjs/typeorm'
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { Product } from './products/product.entity';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 @Module({
-  imports: [UsersModule, ProductsModule, ReviewsModule,
-    TypeOrmModule.forRoot(
-    {
-      type:'postgres',
-      username:'islamabdallah',
-      database:'postgres',
-      password:'',
-      port:5432,
-      host:'localhost',
-      synchronize:true,
-      entities:[Product]
-    }
-  )
-],
+  imports: [
+    UsersModule,
+    ProductsModule,
+    ReviewsModule,
+    // ConfigModule.forRoot({
+    //   isGlobal: true,
+    //   envFilePath: '.env.local'
+    // }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      username: 'islamabdallah',
+      database: 'postgres',
+      password: '',
+      port: 5432,
+      host: 'localhost',
+      synchronize: true,
+      entities: [Product],
+    }),
+    // TypeOrmModule.forRootAsync({
+    //   imports: [ConfigModule],
+    //   inject: [ConfigService],
+    //   useFactory: (config: ConfigService) => {
+    //     return {
+    //     type: 'postgres',
+    //     host: config.get<string>('DB_HOST') ?? '127.0.0.1',
+    //     port: Number(config.get<string>('DB_PORT') ?? '5432'),
+    //     username: config.get<string>('DB_USERNAME'),
+    //     password: config.get<string>('DB_PASSWORD') || undefined, // allow empty
+    //     database: config.get<string>('DB_DATABASE'),
+    //     synchronize: true,                    // dev only!
+    //     autoLoadEntities: true,               // no need to list entities manually
+    //     // entities: [Product],               // remove if using autoLoadEntities
+    //     }
+    //   }
+
+    // }
+
+    // ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
